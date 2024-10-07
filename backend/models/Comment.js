cf05f7b387d632f8f1919a59b4cf838d9d42e15d
seula <urghb12375@gmail.com>
2024-10-07 21:33:41 +0900

    오타 수정

--- backend/models/Comment.js
+++ backend/models/Comment.js
@@ -1,9 +1,9 @@
 import mongoose from "mongoose";
 
 const commentSchema = new mongoose.Schema({
-  post: { type: mongoose.Schema.Types.ObjectId, ref: "Post", reqired: true }, //댓글 달린 게시물
-  user: { type: mongoose.Schema.Types.ObjectId, ref: "User", reqired: true }, //댓글을 작성한 사용자
-  text: { type: String, reqired: true }, //댓글내용
+  post: { type: mongoose.Schema.Types.ObjectId, ref: "Post", required: true }, //댓글 달린 게시물
+  user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true }, //댓글을 작성한 사용자
+  text: { type: String, required: true }, //댓글내용
   date: { type: Date, default: Date.now },
 });
 
--- backend/models/Post.js
+++ backend/models/Post.js
@@ -1,9 +1,9 @@
 import mongoose from "mongoose";
 //게시물 정보를 저장할 모델 정의
 const postSchema = new mongoose.Schema({
-  title: { type: String, reqired: true },
-  content: { type: String, reqired: true },
-  author: { type: mongoose.Schema.Types.ObjectId, ref: "User", reqired: true },
+  title: { type: String, required: true },
+  content: { type: String, required: true },
+  author: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
   date: { type: Date, default: Date.now },
 });
 

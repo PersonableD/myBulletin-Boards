cc6f8cab4cdb4bc08119948413f412456e980685
seula <urghb12375@gmail.com>
2024-10-07 17:57:42 +0900

    게시물 수정/삭제 라우터 구현

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -20,7 +20,44 @@ router.post("/create", auth, async (req, res) => {
     res.status(500).json({ msg: "Server error" });
   }
 });
-router.put("/edit/:id", auth, (req, res) => {});
-router.delete("/delete", auth, (req, res) => {});
+router.put("/edit/:id", auth, async (req, res) => {
+  const { title, content } = req.body;
+  try {
+    //라우터에 :id 이부분을 가지고 게시물을 찾음/수정하면 이미 있는 게시물이여야 함
+    let post = await Post.findById(req.params.id);
+    //게시물이 없다면 게시물을 찾을 수 없다는 메세지 띄움
+    if (!post) {
+      return res.status(404).json({ msg: "Post not found" });
+    }
+    //게시물 작성자 == 수정하려는 작성자인지?
+    if (post.author.toString() !== req.user.id) {
+      return res.status(404).json({ msg: "User not authorized" });
+    }
+    //게시물 업데이트
+    post.title = title || post.title;
+    post.content = content || post.content;
+    await post.save();
+  } catch (error) {
+    res.status(500).json({ msg: "Server error" });
+  }
+});
+router.delete("/delete/:id", auth, async (req, res) => {
+  try {
+    let post = await Post.findById(req.params.id);
+    if (!post) {
+      return res.status(404).json({ msg: "Post not found" });
+    }
+    //게시물 작성자 == 삭제하려는 작성자인지?
+    if (post.author.toString() !== req.user.id) {
+      return res.status(401).json({ msg: "User not authorized" });
+    }
+    //여기 remove()로 했다가 안되서 deleteOne()으로 바꾸니까 됨
+    await post.deleteOne();
+    res.json({ msg: "Post removed" });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ msg: "delete error" });
+  }
+});
 
 export default router;

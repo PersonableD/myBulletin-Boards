c42059f82e5c634dca9e7039c18abab97c20d906
seula <urghb12375@gmail.com>
2024-10-09 18:40:09 +0900

    json파일 업로드

--- backend/routes/comments.js
+++ backend/routes/comments.js
@@ -4,7 +4,7 @@ import Comment from "../models/Comment.js";
 import Post from "../models/Post.js";
 
 const router = express.Router();
-
+//댓글 불러오기 라우터
 router.get("/:postId", async (req, res) => {
   try {
     const comments = await Comment.find({ post: req.params.postId });
@@ -13,7 +13,7 @@ router.get("/:postId", async (req, res) => {
     res.status(500).json({ msg: "댓글 불러오기 오류" });
   }
 });
-
+//댓글 작성 라우터
 router.post("/:postId", auth, async (req, res) => {
   const { text } = req.body;
   try {
@@ -32,6 +32,25 @@ router.post("/:postId", auth, async (req, res) => {
     res.status(201).json(comment);
   } catch (error) {
     res.status(500).json({ msg: "Server error" });
+  }
+});
+//댓글 삭제 라우터
+router.delete("/:commentId", auth, async (req, res) => {
+  try {
+    let post = await Comment.findById(req.params.commentId);
+    if (!post) {
+      return res.status(404).json({ msg: "Post not found" });
+    }
+    //게시물 작성자 == 삭제하려는 작성자인지?
+    if (post.author.toString() !== req.user.id) {
+      return res.status(401).json({ msg: "User not authorized" });
+    }
+    //여기 remove()로 했다가 안되서 deleteOne()으로 바꾸니까 됨
+    await post.deleteOne();
+    res.json({ msg: "Post removed" });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ msg: "delete error" });
   }
 });
 

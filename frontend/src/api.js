f877f52e1aa8ed4e96320fb83d4f7e2f8ee6a82b
seula <urghb12375@gmail.com>
2024-10-09 19:01:04 +0900

    배포용 프론트엔드 환경변수 설정

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api", // 백엔드 URL
+  baseURL: process.env.REACT_APP_BACKEND_URL, // 백엔드 URL
 });
 
 export default api;
